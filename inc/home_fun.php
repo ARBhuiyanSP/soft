a15c463e9e4feedec72bba6fcc06ca975bc133e4
ARBhuiyanSP <arbhuiyan.pits@gmail.com>
2021-02-15 13:12:01 +0600

    first commit

--- inc/home_fun.php
+++ inc/home_fun.php
@@ -1,57 +1,15 @@
 <div class="fun-staff staff-1 block-bg-2 sectionWrapper">
 	<div class="container team-boxes">
 					<h3 class="block-head" style="color:#fff;font-weight:bold">Meet Our Software Team</h3>
-						<div class="cell-3 fx" data-animate="bounceIn">
-							<div class="team-box">
-								<div class="team-img">
-									<img alt="" src="images/people/tmd-ruhul-saif.png">
-									<h3 style="color:#000;font-weight:bold;text-align:center;">T.MD.RUHUL SAIF</h3>
-								</div>
-								<div class="team-details">
-									<h3 class="gry-bg">T.MD.RUHUL SAIF</h3>
-									<h5 style="padding:5px;"><center>DIRECTOR</center></h5>
-									<div class="team-socials">
-										<ul>
-											<li><a href="#" title="facebook"><span class="fa fa-facebook"></span></a></li>
-											<li><a href="#" title="linkedin"><span class="fa fa-linkedin"></span></a></li>
-											<li><a href="#" title="skype"><span class="fa fa-skype"></span></a></li>
-											<li><a href="#" title="twitter"><span class="fa fa-twitter"></span></a></li>
-											<li><a href="#" title="vimeo"><span class="fa fa-google-plus"></span></a></li>
-										</ul>
-									</div>
-								</div>
-							</div>
-						</div>
-						<!-- <div class="cell-3 fx" data-animate="bounceIn">
-							<div class="team-box">
-								<div class="team-img">
-									<img alt="" src="images/people/1.jpg">
-									<h5 style="color:#000;font-weight:bold;text-align:center;">Dewan Rubaiat Hussain</h5>
-								</div>
-								<div class="team-details">
-									<h3 class="gry-bg">Dewan Rubaiat Hussain</h3>
-									<h5 style="padding:5px;"><center>Head of IT</center></h5>
-									<div class="team-socials">
-										<ul>
-											<li><a href="#" title="facebook"><span class="fa fa-facebook"></span></a></li>
-											<li><a href="#" title="linkedin"><span class="fa fa-linkedin"></span></a></li>
-											<li><a href="#" title="skype"><span class="fa fa-skype"></span></a></li>
-											<li><a href="#" title="twitter"><span class="fa fa-twitter"></span></a></li>
-											<li><a href="#" title="vimeo"><span class="fa fa-google-plus"></span></a></li>
-										</ul>
-									</div>
-								</div>
-							</div>
-						</div> -->
 						<div class="cell-3 fx" data-animate="bounceIn" data-animation-delay="600">
 							<div class="team-box">
 								<div class="team-img">
-									<img alt="" src="images/people/1.jpg">
-									<h5 style="color:#000;font-weight:bold;text-align:center;">Fakhrul Islam Polash</h5>
+									<img alt="" src="images/people/tarafder-niger-sultana.png">
+									<h5 style="color:#000;font-weight:bold;text-align:center;">TARAFDER NIGAR SULTANA</h5>
 								</div>
 								<div class="team-details">
-									<h3 class="gry-bg">Fakhrul Islam Polash</h3>
-									<h5 style="padding:5px;"><center>Manager, IT</center></h5>
+									<h3 class="gry-bg">TARAFDER NIGAR SULTANA</h3>
+									<h5 style="padding:5px;"><center>CHAIRMAN</center></h5>
 									<div class="team-socials">
 										<ul>
 											<li><a href="#" title="facebook"><span class="fa fa-facebook"></span></a></li>
@@ -67,12 +25,12 @@
 						<div class="cell-3 fx" data-animate="bounceIn" data-animation-delay="200">
 							<div class="team-box">
 								<div class="team-img">
-									<img alt="" src="images/people/1.jpg">
-									<h5 style="color:#000;font-weight:bold;text-align:center;">Tanveer Qurashee</h5>
+									<img alt="" src="images/people/tmd-ruhul-amin.png">
+									<h5 style="color:#000;font-weight:bold;text-align:center;">T.MD.RUHUL AMIN</h5>
 								</div>
 								<div class="team-details">
-									<h3 class="gry-bg">Tanveer Qurashee</h3>
-									<h5 style="padding:5px;"><center>Senior Software Engineer</center></h5>
+									<h3 class="gry-bg">T.MD.RUHUL AMIN</h3>
+									<h5 style="padding:5px;"><center>MANAGING DIRECTOR</center></h5>
 									<div class="team-socials">
 										<ul>
 											<li><a href="#" title="facebook"><span class="fa fa-facebook"></span></a></li>
@@ -88,12 +46,33 @@
 						<div class="cell-3 fx" data-animate="bounceIn" data-animation-delay="400">
 							<div class="team-box">
 								<div class="team-img">
-									<img alt="" src="images/people/1.jpg">
-									<h5 style="color:#000;font-weight:bold;text-align:center;">Atiqur Rahman Bhuiyan</h5>
+									<img alt="" src="images/people/rubya-chowdhury.png">
+									<h5 style="color:#000;font-weight:bold;text-align:center;">RUBYA CHOWDHURY</h5>
 								</div>
 								<div class="team-details">
-									<h3 class="gry-bg">Atiqur Rahman Bhuiyan</h3>
-									<h5 style="padding:5px;"><center>Software Engineer</center></h5>
+									<h3 class="gry-bg">RUBYA CHOWDHURY</h3>
+									<h5 style="padding:5px;"><center>DIRECTOR</center></h5>
+									<div class="team-socials">
+										<ul>
+											<li><a href="#" title="facebook"><span class="fa fa-facebook"></span></a></li>
+											<li><a href="#" title="linkedin"><span class="fa fa-linkedin"></span></a></li>
+											<li><a href="#" title="skype"><span class="fa fa-skype"></span></a></li>
+											<li><a href="#" title="twitter"><span class="fa fa-twitter"></span></a></li>
+											<li><a href="#" title="vimeo"><span class="fa fa-google-plus"></span></a></li>
+										</ul>
+									</div>
+								</div>
+							</div>
+						</div>
+						<div class="cell-3 fx" data-animate="bounceIn">
+							<div class="team-box">
+								<div class="team-img">
+									<img alt="" src="images/people/tmd-ruhul-saif.png">
+									<h3 style="color:#000;font-weight:bold;text-align:center;">T.MD.RUHUL SAIF</h3>
+								</div>
+								<div class="team-details">
+									<h3 class="gry-bg">T.MD.RUHUL SAIF</h3>
+									<h5 style="padding:5px;"><center>DIRECTOR</center></h5>
 									<div class="team-socials">
 										<ul>
 											<li><a href="#" title="facebook"><span class="fa fa-facebook"></span></a></li>
