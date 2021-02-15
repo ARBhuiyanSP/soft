cb422c2c0778864aed95069be9678599dd3d29a2
ARBhuiyanSP <arbhuiyan.pits@gmail.com>
2021-02-15 12:36:25 +0600

    first commit

--- inc/home_slider.php
+++ inc/home_slider.php
@@ -24,14 +24,14 @@
 								data-endelementdelay="0.1"
 								data-endspeed="500"
 								data-endeasing="Power4.easeIn"></div>
-								<div class="caption lfl large-text tp-resizeme witTxt" style="text-transform: uppercase" data-x="480" data-y="200" data-speed="600" data-start="2500" data-easing="Power4.easeOut"
+								<div class="caption lfl large-text tp-resizeme witTxt" style="text-transform: uppercase" data-x="450" data-y="200" data-speed="600" data-start="2500" data-easing="Power4.easeOut"
 											data-splitin="chars"
 											data-splitout="chars"
 											data-elementdelay="0.05"
 											data-endelementdelay="0.05"
 											data-endspeed="300"
 											data-endeasing="Power1.easeOut"
-								>Follow Up Your</div>
+								>Give Support To Our</div>
 								<div class="caption lfl large-title tp-resizeme witTxt" style="font-size:40px" data-x="450" data-y="240" data-speed="600" data-start="2800" data-easing="Power4.easeOut"
 											data-splitin="chars"
 											data-splitout="chars"
@@ -40,24 +40,23 @@
 											data-endspeed="300"
 											data-endeasing="Power1.easeOut"
 								>Business</div>
-								<div class="caption lfl large-text tp-resizeme witTxt" style="text-transform: uppercase" data-x="480" data-y="310" data-speed="600" data-start="3100" data-easing="Power4.easeOut"
+								<div class="caption lfl large-text tp-resizeme witTxt" style="text-transform: uppercase" data-x="450" data-y="310" data-speed="600" data-start="3100" data-easing="Power4.easeOut"
 											data-splitin="chars"
 											data-splitout="chars"
 											data-elementdelay="0.05"
 											data-endelementdelay="0.05"
 											data-endspeed="300"
 											data-endeasing="Power1.easeOut"
-								>wherever you are</div>
+								>wherever we are</div>
 								<div class="caption lfl large-text tp-resizeme witTxt customin fadeout " style="text-transform: uppercase" data-x="658" data-y="200" data-speed="600" data-start="2200" data-customin="x:0;y:0;z:0;rotationX:0;rotationY:0;rotationZ:0;scaleX:0;scaleY:0;skewX:0;skewY:0;opacity:0;transformPerspective:600;transformOrigin:50% 50%;"
 ><i class="fa fa-anchor main-bg" style="border:5px #fff solid;padding:30px;font-size:60px;line-height:45px;border-radius:50%"></i></div>
 								<div class="caption lfr large-title tp-resizeme main-color" style="font-size:40px" data-x="right" data-y="140" data-speed="800" data-start="2800"><span style="font-weight:100 !important;color:#fff">We </span>SAIF POWERTEC LTD</div>
 								<div class="caption sfr large-text tp-resizeme witTxt" style="font-size:16px" data-x="right" data-y="200" data-speed="600" data-start="3200">
-								Wherever you are you will be<br>
-								able to view monitor your<br>
-								website easily on your <br>
-								mobile with the most user<br>
-								friendly theme you will ever <br>
-								see, it is <b>EXCEPTION</b>. 
+								Saif Software Development <br>
+								Team is a group of Experts <br>
+								in Software Development, <br>
+								 SEO, Graphic Design and<br>
+								 Web Design & Developments. 
 								</div>
 							</li>
 						</ul>
